64dcaef42484a02e647957ccc7235abb5b9b4088
Pietot <102545552+Pietot@users.noreply.github.com>
2024-07-08 23:40:32 +0200

    Minor change

--- single neuron/single_neuron_scratch.py
+++ single neuron/single_neuron_scratch.py
@@ -21,6 +21,7 @@ class NeuralNetwork:
         self.learning_rate = learning_rate
         self.losses: list[np.floating[Any]] = []
         self.accuracies: list[np.floating[Any]] = []
+        self.training_time: float = 0.0
 
     def activation(self, Z: npt.NDArray[np.float64]) -> np.ndarray[Any, np.dtype[Any]]:
         """Activation function using ReLU
@@ -105,7 +106,16 @@ class NeuralNetwork:
         start = time.time()
         for _ in tqdm(range(self.nb_epoch)):
             self.update()
-            self.accuracies.append(round(np.mean(np.argmax(self.forward_propagation(self.train_matrix), axis=0) == np.argmax(self.answer, axis=0)) * 100, 2))  # type: ignore
+            self.accuracies.append(
+                round(
+                    np.mean(  # type: ignore
+                        np.argmax(self.forward_propagation(self.train_matrix), axis=0)
+                        == np.argmax(self.answer, axis=0)
+                    )
+                    * 100,
+                    2,
+                )
+            )
         self.training_time = round(time.time() - start, 3)
         return (self.vector_weight, self.bias)
 
