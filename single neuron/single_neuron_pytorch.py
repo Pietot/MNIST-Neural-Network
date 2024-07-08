c850f6a6e700647b3c433d9bd85b61fdd702ad04
Pietot <102545552+Pietot@users.noreply.github.com>
2024-07-09 00:37:19 +0200

    Started playing with Pytorch to learnthe framwork

--- single neuron/single_neuron_pytorch.py
+++ single neuron/single_neuron_pytorch.py
@@ -3,6 +3,9 @@ import torch
 import torchvision
 import pickle
 
+from torchvision.datasets.mnist import MNIST  # type: ignore
+from torch.utils.data import DataLoader
+
 from PIL import Image
 from tqdm import tqdm
 from typing import Any
@@ -23,24 +26,46 @@ class NeuralNetwork:
         self.training_time: float = 0.0
 
 
-def load_train_mnist() -> :
+def load_train_mnist() -> MNIST:
     """Load the training MNIST dataset
 
     Returns:
-        : The training dataset
+        The training dataset
     """
     train_dataset = torchvision.datasets.MNIST(
-        root="/",
+        root="C:\\Users\\Bapti\\OneDrive\\Bureau\\VS Code/Python\
+\\Solo Project\\Medium Projects\\Autre\\MNIST Neural Network",
         train=True,
         transform=torchvision.transforms.ToTensor(),
-        download=True
+        download=True,
     )
     return train_dataset
-    train_dataset = torch.reshape(train_dataset, (60000, 784))
 
 
 if __name__ == "__main__":
+    labels_map = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+    }
+
     network = NeuralNetwork()
-    network.train()
-    print(network.training_time)
-    network.test()
+    from matplotlib import pyplot as plt
+
+    figure = plt.figure(figsize=(8, 8))
+    cols, rows = 3, 3
+    for i in range(1, cols * rows + 1):
+        sample_idx = torch.randint(len(network.train_matrix), size=(1,)).item()
+        img, label = network.train_matrix[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title(labels_map[label])
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+    plt.show()
